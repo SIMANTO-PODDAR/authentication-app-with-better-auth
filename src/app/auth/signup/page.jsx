fd9780915c515a48d1b89426385350a45030a65b
Simanto Poddar <251956045+SIMANTO-PODDAR@users.noreply.github.com>
2026-04-22 13:47:38 +0600

    authentication-app-with-better-auth

---
+++ src/app/auth/signup/page.jsx
@@ -0,0 +1,15 @@
+"use client";
+import {Check} from "@gravity-ui/icons";
+import {Button, Description, FieldError, Form, Input, Label, TextField} from "@heroui/react";
+
+
+const SignUpPage = () => {
+    return (
+        <div>
+            <h1 className='text-2xl text-center font-bold'>Sign Up:</h1>
+
+        </div>
+    );
+};
+
+export default SignUpPage;
