c2cb597b1bba58e3201f20cb0fc3dbcea602f9ea
Simanto Poddar <251956045+SIMANTO-PODDAR@users.noreply.github.com>
2026-04-25 15:12:25 +0600

    update link

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
-    baseURL: "authentication-app-with-bet-git-c70644-simanto-poddars-projects.vercel.app"
+    baseURL: "https://authentication-app-with-bet-git-c70644-simanto-poddars-projects.vercel.app/"
     // baseURL: "http://localhost:3000"
 })
 
