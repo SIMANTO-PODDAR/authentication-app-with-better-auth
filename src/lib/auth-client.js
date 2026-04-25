d19df2626de9203f8660ef6ce285e20963256e47
Simanto Poddar <251956045+SIMANTO-PODDAR@users.noreply.github.com>
2026-04-25 16:00:10 +0600

    update link

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
-    baseURL: "https://authentication-app-with-better-auth.vercel.app"
+    baseURL: "https://authentication-app-with-better-auth-simanto-poddars-projects.vercel.app/"
 })
 
 export const { signIn, signUp, useSession } = createAuthClient()
